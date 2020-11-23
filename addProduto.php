89b4e679e72270c972880381e939407a2e9b060b
leandrolannister <leandrohendrix@gmail.com>
2020-11-23 16:34:01 -0300

    Rotina inset

--- addProduto.php
+++ addProduto.php
@@ -1,7 +1,19 @@
-<?php require_once('header.php'); ?>
+<?php require_once('header.php'); 
+  $nome = $_GET['nome'];
+  $preco = $_GET['preco'];
 
-<p class="alert alert-success principal">
-  Produto <?=$_GET['nome']?>. Preço <?=$_GET['preco']?> add com sucesso!
-</p>
+  $conexao = mysqli_connect('127.0.0.1','root','', 'loja');
 
-<?php require_once('footer.php'); ?>
+  $query = "INSERT INTO produtos(nome, preco) values('{$nome}', {$preco})";
+
+  if(mysqli_query($conexao, $query)){ ?>
+    <p class="alert alert-success principal">
+      Produto <?=$_GET['nome']?>. Preço <?=$_GET['preco']?> add com sucesso!
+    </p> 	
+  <?php }else{ ?>
+    <p class="alert alert-danger principal">
+      Produto <?=$_GET['nome']?>. Preço <?=$_GET['preco']?> não foi criado
+    </p>
+  <?php } 
+
+require_once('footer.php'); ?>
---
+++ sql.php
@@ -0,0 +1,6 @@
+CREATE table produtos(id int auto_increment, 
+  nome varchar(255),
+  preco decimal(10,2),
+  primary key(id));
+
+desc produtos;
